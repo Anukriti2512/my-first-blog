03311a35e777db1978f1d08e06d7adbfee5aa833
Anukriti2512 <anukriti251298@gmail.com>
2019-02-10 19:28:33 +0530

    fix error

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,11 +115,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = '/'
 
